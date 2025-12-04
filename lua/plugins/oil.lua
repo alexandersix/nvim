3f940fde8b4404ba87e90febc2445f334072b97e
Alexander Six <alexanderhsix@gmail.com>
2025-12-03 20:37:59 -0500

    feat(oil): show hidden files by default in explorer

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,13 +1,18 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  keys = {
-	  { "<leader>e", "<cmd>Oil<cr>" }
-  },
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
+		default_file_explorer = true,
+		view_options = {
+			show_hidden = true,
+		},
+	},
+	-- Optional dependencies
+	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+	keys = {
+		{ "<leader>e", "<cmd>Oil<cr>" },
+	},
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
 }
